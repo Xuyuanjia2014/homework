c46c245c1215ecf93d0202c140182358bd8c273e
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2018-05-11 18:06:12 +0800

    traverse.

--- soot/src/PersonAnalyst.java
+++ soot/src/PersonAnalyst.java
@@ -1,2 +1,39 @@
+import soot.*;
+import soot.jimple.JimpleBody;
+import soot.options.Options;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class PersonAnalyst {
+    public static void main(String[] args) {
+        System.out.println("good:"+System.getProperty("java.home"));
+        List<String> processDir = new LinkedList<String>();
+        processDir.add("/root/IdeaProjects/soot-tools/soot/target/classes");
+        Options.v().set_process_dir(processDir);
+        System.out.println(Scene.v().getSootClassPath());
+        SootClass sc = Scene.v().loadClassAndSupport("Person");
+        sc.setApplicationClass();
+
+        StringBuffer finalString = new StringBuffer();
+        finalString.append("Person's methods:\n");
+        for(SootMethod temp : sc.getMethods()){
+            StringBuffer sb = new StringBuffer("");
+            for(Type type: temp.getParameterTypes()){
+                if(sb.toString().contains(",")){
+                    sb.append(","+type.toQuotedString());
+                }
+                else {
+                    sb.append(type.toQuotedString());
+                }
+
+            }
+            finalString.append(temp.getReturnType()+" "+temp.getName()+"("+sb.toString()+")\n");
+        }
+        finalString.append("Person's fields:\n");
+        for(SootField temp : sc.getFields()){
+            finalString.append(temp.getDeclaration()+" of "+ temp.getDeclaringClass().toString()+"\n");
+        }
+        System.out.println(finalString.toString());
+    }
 }
